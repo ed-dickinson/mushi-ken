5f89f8bf01227c4617b1ff9aba90c4ffc0b84c8a
ed-dickinson <edwardejd@gmail.com>
2021-07-12 11:35:35 +0100

    logic for game

--- mushi-ken.js
+++ mushi-ken.js
@@ -2,8 +2,17 @@ const frog = 'Frog';
 const slug = 'Slug';
 const snake = 'Snake';
 const frogArray = [frog, slug, snake];
+
 let score = [0, 0]; // you, comp
 let successfullGames = 0;
+
+const frogCard = document.getElementById('frog-card');
+const slugCard = document.getElementById('slug-card');
+const snakeCard = document.getElementById('snake-card');
+const cardArray = [frogCard, slugCard, snakeCard];
+
+const textDisplay = document.getElementById('score-text');
+const scoreDisplay = document.getElementById('score');
 
 function computerPlay() {
     let rand = Math.floor(Math.random() * 2.9999999999);
@@ -28,35 +37,60 @@ function playRound(playerSelection, computerSelection) {
     }
 };
 
-function game() {
-    while (successfullGames < 5) {
-        let playerSelection = prompt("Frog, Slug, or Snake?");
-        let computerSelection = computerPlay();
-        console.log(playRound(playerSelection, computerSelection));
-    }
-    if (score[0] > score[1]) {
-        console.log("You lose. Commiserations.");
-    } else {
-        console.log("You win! Congratulations!");
+function game(playerSelection, computerSelection) {
+//    while (successfullGames < 5) {
+//        let playerSelection = prompt("Frog, Slug, or Snake?");
+//        let computerSelection = computerPlay();
+//        console.log(playRound(playerSelection, computerSelection));
+//    }
+    if (successfullGames == 5 && score[0] > score[1]) {
+        textDisplay.innerHTML = computerSelection + " beats " + playerSelection + ", you lose the game. Commiserations.";
+        score = [0, 0];
+        successfullGames = 0;
+    } else if (successfullGames == 5 && score[0] < score[1]) {
+        textDisplay.innerHTML = playerSelection + " beats " + computerSelection + ", you win the game! Congratulations!";
+        score = [0, 0];
+        successfullGames = 0;
     }
 }
 
 
-
+function flashCard(card) {
+    card.classList.add('comp-selected');
+    console.log(card);
+    let id = null;
+    let flash = 0;
+    clearInterval(id);
+    id = setInterval (frame, 200);
+    function frame() {
+        if (flash == 1) {
+            card.classList.remove('comp-selected');
+            clearInterval(id);
+        } else {
+            flash++;
+        }
+    }
+    
+}
 //game(); 
 
-const frogCard = document.getElementById('frog-card');
-const slugCard = document.getElementById('slug-card');
-const snakeCard = document.getElementById('snake-card');
+function roundChoice(e) {
+//    console.log(e.target);
+    let computerChoice = computerPlay();
+//    console.log(frogArray.indexOf(computerChoice));
+    let card = cardArray[frogArray.indexOf(computerChoice)];
+    console.log(card);
+    flashCard(card);
+    
+//    let playerCard = cardArray.indexOf(e.target);
+    let playerChoice = frogArray[cardArray.indexOf(e.target)]
+    textDisplay.innerHTML = playRound(playerChoice, computerChoice);
+    scoreDisplay.innerHTML = score[0] + ' - ' + score[1];
+    game(playerChoice, computerChoice);
+    
+}
 
-const textDisplay = document.getElementById('below-text');
 
-frogCard.addEventListener('click', function() {
-    textDisplay.innerHTML = playRound(frog, computerPlay());
-});
-slugCard.addEventListener('click', function() {
-    textDisplay.innerHTML = playRound(slug, computerPlay());
-});
-snakeCard.addEventListener('click', function() {
-    textDisplay.innerHTML = playRound(snake, computerPlay());
-});
+frogCard.addEventListener('click', roundChoice);
+slugCard.addEventListener('click', roundChoice);
+snakeCard.addEventListener('click', roundChoice);
